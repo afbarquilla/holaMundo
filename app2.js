6b5aa0b2e73d7fe1e75cc31ddfe59bacea2dfd2b
unknown <afbarquilla@gmail.com>
2021-11-22 00:32:32 +0100

    mi segundo commit

--- app2.js
+++ app2.js
@@ -6,7 +6,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo2');
+  res.end('Hola Mundo3');
 });
 
 server.listen(port, hostname, () => {
